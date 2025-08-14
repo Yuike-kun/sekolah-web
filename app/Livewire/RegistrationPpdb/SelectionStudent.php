eb4491fe5bbed3afbac6ef5a26ff7f9d32763887
Yuike-kun <akbarhuntermob@gmail.com>
2025-08-14 16:12:21 +0800

    [Updated] SelectionStudent is now synch

--- app/Livewire/RegistrationPpdb/SelectionStudent.php
+++ app/Livewire/RegistrationPpdb/SelectionStudent.php
@@ -1,17 +1,64 @@
 <?php
-
 namespace App\Livewire\RegistrationPpdb;
 
+use App\Livewire\Traits\DataTable\WithBulkActions;
+use App\Livewire\Traits\DataTable\WithCachedRows;
+use App\Livewire\Traits\DataTable\WithPerPagePagination;
+use App\Livewire\Traits\DataTable\WithSorting;
+use App\Models\PPDB;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class SelectionStudent extends Component
 {
+    use WithBulkActions;
+    use WithCachedRows;
+    use WithPerPagePagination;
+    use WithSorting;
+
+    public $filters = [
+        'search'                  => '',
+        'verification_graduation' => '',
+        'date_start'              => '',
+        'date_end'                => '',
+        'nisn'                    => '',
+    ];
+
     #[Title('Hasil Seleksi')]
     #[Layout('layouts.landing-page')]
+
+    public function getRowsQueryProperty()
+    {
+        $query = PPDB::query()
+            ->whereHas('student', function ($query) {
+                $query
+                    ->where('verification_student', 'Terverifikasi')
+                    ->when(! $this->sorts, fn($query) => $query->latest())
+                    ->when($this->filters['search'], function ($query, $search) {
+                        $query->where('nik', 'LIKE', "%$search%")
+                            ->orWhere('nisn', 'LIKE', "%$search%")
+                            ->orWhere('full_name', 'LIKE', "%$search%")
+                            ->orWhere('number_registration', 'LIKE', "%$search%");
+                    })
+                    ->when($this->filters['nisn'], fn($query, $nisn) => $query->where('nisn', $nisn))
+                    ->when($this->filters['verification_graduation'], fn($query, $status) => $query->where('verification_graduation', $status))
+                    ->when($this->filters['date_start'], fn($query, $date) => $query->where('created_at', '>=', $date))
+                    ->when($this->filters['date_end'], fn($query, $date) => $query->where('created_at', '<=', $date));
+            });
+
+        return $this->applyPagination($query);
+    }
+
+    public function resetFilter()
+    {
+        $this->reset();
+    }
+
     public function render()
     {
-        return view('livewire.registration-ppdb.selection-student');
+        return view('livewire.registration-ppdb.selection-student', [
+            'items' => $this->getRowsQueryProperty(),
+        ]);
     }
 }
--- resources/views/livewire/registration-ppdb/selection-student.blade.php
+++ resources/views/livewire/registration-ppdb/selection-student.blade.php
@@ -1,52 +1,51 @@
 <div class="container my-5">
     <div class="card p-5">
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th>Nama Siswa</th>
-                    <th>NISN</th>
-                    <th>NIK</th>
-                    <th>Status Pendaftaran</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Muhammad Bintang</td>
-                    <td>0999383939993</td>
-                    <td>0999383939772627</td>
-                    <td>Lulus</td>
-                </tr>
-                <tr>
-                    <td>Muhammad Bintang</td>
-                    <td>0999383939993</td>
-                    <td>0999383939772627</td>
-                    <td>Lulus</td>
-                </tr>
-                <tr>
-                    <td>Muhammad Bintang</td>
-                    <td>0999383939993</td>
-                    <td>0999383939772627</td>
-                    <td>Lulus</td>
-                </tr>
-                <tr>
-                    <td>Muhammad Bintang</td>
-                    <td>0999383939993</td>
-                    <td>0999383939772627</td>
-                    <td>Lulus</td>
-                </tr>
-                <tr>
-                    <td>Muhammad Bintang</td>
-                    <td>0999383939993</td>
-                    <td>0999383939772627</td>
-                    <td>Lulus</td>
-                </tr>
-                <tr>
-                    <td>Muhammad Bintang</td>
-                    <td>0999383939993</td>
-                    <td>0999383939772627</td>
-                    <td>Lulus</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="table-responsive card" wire:loading.class.delay="card-loading" wire:offline.class="card-loading">
+            <table id="example" class="table table-bordered" style="width:100%">
+                <thead>
+                    <tr>
+                        <th class="text-center">No</th>
+                        <th>
+                            <x-datatable.column-sort name="No. Pendaftaran" wire:click="sortBy('number_registration')"
+                                :direction="$sorts['number_registration'] ?? null" />
+                        </th>
+                        <th>
+                            <x-datatable.column-sort name="NISN" wire:click="sortBy('nisn')" :direction="$sorts['nisn'] ?? null" />
+                        </th>
+                        <th>
+                            <x-datatable.column-sort name="NIK" wire:click="sortBy('nik')" :direction="$sorts['nik'] ?? null" />
+                        </th>
+                        <th>
+                            <x-datatable.column-sort name="Nama Lengkap" wire:click="sortBy('full_name')"
+                                :direction="$sorts['full_name'] ?? null" />
+                        </th>
+                        <th>
+                            <x-datatable.column-sort name="Status Kelulusan"
+                                wire:click="sortBy('verification_graduation')" :direction="$sorts['verification_graduation'] ?? null" />
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($items as $item)
+                        <tr wire:key="{{ $loop->index }}">
+                            <td class="text-center">{{ $loop->iteration }}</td>
+                            <td>{{ $item->student->number_registration }}</td>
+                            <td>{{ $item->student->nisn }}</td>
+                            <td>{{ $item->student->nik }}</td>
+                            <td>{{ $item->student->full_name }}</td>
+                            <td>
+                                <div
+                                    class="{{ $item->student->verification_graduation == 'Proses' ? 'animated-dots' : '' }} {{ ($item->student->verification_graduation == 'Proses' ? 'badge-primary' : $item->student->verification_graduation == 'Tidak Lulus') ? 'badge-danger' : 'badge-success' }}">
+                                    {{ $item->student->verification_graduation }}
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <x-datatable.empty colspan="7" />
+                    @endforelse
+
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
